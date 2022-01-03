45bc2da9aac5302333ea47f897b826f1b72eb1d3
medaminembk <mbarekmouhamedamine@gmail.com>
2022-01-03 23:31:59 +0100

    'checkpoint_jsx'

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
-
+import './style.css';
+import imageSrc from './imageInSrc.jpg'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{ border: "solid 1px black", maxWidth: '100vw' }}>
+
+        <h1 className="title red">Your name here</h1>
+
+
+
+        <img src={imageSrc} alt='myImage2' style={{width:'200px', height:'400px'}} />
+<br/>
+
+
+        <img src="/images/imageInPublic.jpg" alt='myImage' style={{width:'200px', height:'400px'}} />
+
+      </div>
+
+      {/* <video width="320" height="240" controls>
+
+                <source src="myVideo.mp4" type="video/mp4" />
+
+              </video> */}
     </div>
   );
 }
